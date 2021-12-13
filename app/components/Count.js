8c3e318ef95d4fa880f2d434eb7af78a2d43b686
Hugo Boudalier <hugo.boudalier@edu.devinci.fr>
2021-12-13 14:38:42 +0100

    add navbar

--- app/components/Count.js
+++ app/components/Count.js
@@ -1,36 +1,37 @@
-// import react, {useEffect, useState} from 'react'
+
+
+// import React, { useState, useEffect } from 'react';
 
 // export default function Count() {
-//     const [todolist, setTodoList] = useState([
-//         "toto",
-//         "pute",
-//         "oui"
-//     ]);
-//     const [todo, setTodo] = useState('');
-//     const removeTodo = todo => {
-//         setTodoList(todolist.filter(t => t != todo));
-//     }
+//     const [todo, setTodo] =  useState('')
+//     const [todolist, setTodolist] =  useState([])
+//     const [lastitem, setlastitem] = useState(null);
+
+//     useEffect(() => {
+//         setlastitem(todolist.length - 1);
+//     }, [todolist])
+
+//     useEffect(() => {
+//         localStorage.setItem("todos", todolist)
+//     }, [todolist])
+
+ 
 
 //     return (
 //         <div>
-//             <h1>Todo List :</h1>
-
-//             <input value={todo} onChange={(e) => setTodo(e.target.value)} />
-//             <button onClick={() => setTodoList([ ... todolist, todo])}>Add</button>
-//             {todolist.map(todo => (
-//                 <div>
-//                     <h2>{todo}</h2>
-//                     <button onClick={() => removeTodo(todo)}>Supprimer</button>
-//                 </div>
-
-//             ))}
+          
+//             <h1>To do :</h1>
+//             { todolist.map((item, index) => {
+//                 return (
+//                     <p style={{color : index === lastitem ? 'red' : 'black'}} key={index}>{item} <span onClick={() => {deleteTodo(index)}}></span></p>
+//                 )
+//             })}
+//             <input type="text"
+//  onChange={event => setTodo(event.target.value)} value={todo}/>
+//             <button onClick={() => (setTodolist(todolist => [...todolist, todo]), setTodo(''))}>Add Todo</button>
 //         </div>
-//     );
-
-//     useEffect(() => {
-//         console.log('hello')
-//     })
-// };
+//     )
+// }
 
 import React, { useState, useEffect } from 'react';
 
---
+++ app/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Navbar.module.css'
+
+export default function Navbar() {
+   
+    return (
+        <div className={styles.Navcontainer}>
+          <p>salut</p>
+          <p>les</p>
+          <p>amis</p>
+    
+        </div>
+    )
+}
